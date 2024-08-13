9ed0d8fd5dd94350c5f45e6fe2ee6c4af65b193a
strelchenko <strelchenko.dev@gmail.com>
2024-08-13 15:21:02 +0300

    added build

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,22 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import svgr from 'vite-plugin-svgr';
 import glsl from 'vite-plugin-glsl';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
     plugins: [
         react(),
         glsl(),
-        svgr()
+        svgr(),
+        dts({
+            tsconfigPath: './tsconfig.build.json'
+        })
     ],
     build: {
         lib: {
-            entry: 'src/index.ts',
-            name: 'DiagramLib',
-            fileName: (format) => `diagram-lib.${format}.js`,
+            entry: path.resolve(__dirname, 'src/index.ts'),
+            name: 'diagram-lib',
+            fileName: (format) => `${format}/index.js`,
         },
         rollupOptions: {
             external: [
